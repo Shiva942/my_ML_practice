08a6441750880200f7866d1677f32caa4620f65d
PhoneixChigga <72392914+PhoneixChigga@users.noreply.github.com>
2021-10-15 09:22:40 +0530

    Update assi_3.py

--- assi_3.py
+++ assi_3.py
@@ -17,8 +17,7 @@ def fib(n):
         return fib(n-1)+fib(n-2)
 for i in range (1,var+1):
     print (fib(i))
-
-print('Hello')
+    
 # This repo contains an introduction to [Jupyter](https://jupyter.org) and [IPython](https://ipython.org).
 # 
 # Outline of some basics:
